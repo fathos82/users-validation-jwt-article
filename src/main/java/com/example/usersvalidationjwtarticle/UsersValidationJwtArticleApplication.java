28426e294b0c52f6fe5b0d9010bd2ee2030d521e
Athos Ferreira <fathos82@gmail.com>
2024-07-18 16:24:43 -0300

    Adding Rest.

---
+++ src/main/java/com/example/usersvalidationjwtarticle/UsersValidationJwtArticleApplication.java
@@ -0,0 +1,14 @@
+package com.example.usersvalidationjwtarticle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class
+UsersValidationJwtArticleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsersValidationJwtArticleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/usersvalidationjwtarticle/UsersValidationJwtArticleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.usersvalidationjwtarticle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersValidationJwtArticleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
